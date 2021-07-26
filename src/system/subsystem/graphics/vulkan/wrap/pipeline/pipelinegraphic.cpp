a22e380f147195c0c2adb9ca24e813ce6de7f287
videcode <videcode.link@gmail.com>
2021-07-26 23:47:52 +0300

    функції заготовки для свторення станів конвеєру

--- src/system/subsystem/graphics/vulkan/wrap/pipeline/pipelinegraphic.cpp
+++ src/system/subsystem/graphics/vulkan/wrap/pipeline/pipelinegraphic.cpp
@@ -20,12 +20,64 @@ void PipelineGraphic::createInfoInit(){
 	this->createInfo.sType = vk::StructureType::eGraphicsPipelineCreateInfo;
 	this->createInfo.pNext = nullptr;
 	this->createInfo.flags = vk::PipelineCreateFlagBits::eDisableOptimization;
-	this->createInfo.stageCount = 1;
 
 	this->createInfoStageShader();
+	this->createInfoStateVertexInput();
+	this->createInfoStateInputAssembly();
+	this->createInfoStateTessellation();
+	this->createInfoStateViewport();
+	this->createInfoStateRasterization();
+	this->createInfoStateMultisample();
+	this->createInfoStateDepthStencil();
+	this->createInfoStateColorBlend();
+	this->createInfoStateDynamic();
+/*
+	this->createInfo.layout = ;
+	this->createInfo.renderPass = ;
+	this->createInfo.subpass = ;
+	this->createInfo.basePipelineHandle = ;
+	this->createInfo.basePipelineIndex = ;
+*/
 }
 
 void PipelineGraphic::createInfoStageShader(){
+	this->createInfo.stageCount = 1;
+
+}
+
+void PipelineGraphic::createInfoStateVertexInput(){
+
+}
+
+void PipelineGraphic::createInfoStateInputAssembly(){
+
+}
+
+void PipelineGraphic::createInfoStateTessellation(){
+
+}
+
+void PipelineGraphic::createInfoStateViewport(){
+
+}
+
+void PipelineGraphic::createInfoStateRasterization(){
+
+}
+
+void PipelineGraphic::createInfoStateMultisample(){
+
+}
+
+void PipelineGraphic::createInfoStateDepthStencil(){
+
+}
+
+void PipelineGraphic::createInfoStateColorBlend(){
+
+}
+
+void PipelineGraphic::createInfoStateDynamic(){
 
 }
 
