0b00b67d9bf6c9caf87959577a0604c17503eabf
videcode <videcode.link@gmail.com>
2021-07-28 22:57:57 +0300

    підготовка функцій для створення конвеєру

--- src/system/subsystem/graphics/vulkan/wrap/pipeline/pipelinegraphic.cpp
+++ src/system/subsystem/graphics/vulkan/wrap/pipeline/pipelinegraphic.cpp
@@ -31,54 +31,109 @@ void PipelineGraphic::createInfoInit(){
 	this->createInfoStateDepthStencil();
 	this->createInfoStateColorBlend();
 	this->createInfoStateDynamic();
-/*
-	this->createInfo.layout = ;
-	this->createInfo.renderPass = ;
-	this->createInfo.subpass = ;
-	this->createInfo.basePipelineHandle = ;
-	this->createInfo.basePipelineIndex = ;
-*/
+
+	this->createInfo.layout = VK_NULL_HANDLE;
+	//this->createInfo.renderPass = ;
+	this->createInfo.subpass = 0;
+	this->createInfo.basePipelineHandle = VK_NULL_HANDLE;
+	this->createInfo.basePipelineIndex = 0;
 }
 
 void PipelineGraphic::createInfoStageShader(){
 	this->createInfo.stageCount = 1;
+	this->stageShaderInfo.sType = vk::StructureType::ePipelineShaderStageCreateInfo;
+	this->stageShaderInfo.pNext = nullptr;
+	this->stageShaderInfo.flags = {};
+	this->stageShaderInfo.stage = vk::ShaderStageFlagBits::eVertex;
+	//this->stageShaderInfo.module = ;
+	this->stageShaderInfo.pName = "main";
+	this->stageShaderInfo.pSpecializationInfo = nullptr;
 
+	this->createInfo.pStages = &this->stageShaderInfo;
 }
 
 void PipelineGraphic::createInfoStateVertexInput(){
+	this->stateVertexInput.sType = vk::StructureType::ePipelineVertexInputStateCreateInfo;
+	this->stateVertexInput.pNext = nullptr;
+	this->stateVertexInput.flags = {};
+	this->stateVertexInput.vertexBindingDescriptionCount	= 0;
+	this->stateVertexInput.pVertexBindingDescriptions		= nullptr;
+	this->stateVertexInput.vertexAttributeDescriptionCount	= 0;
+	this->stateVertexInput.pVertexAttributeDescriptions		= nullptr;
 
+	this->createInfo.pVertexInputState = &this->stateVertexInput;
 }
 
 void PipelineGraphic::createInfoStateInputAssembly(){
+	this->stateInputAssembly.sType = vk::StructureType::ePipelineInputAssemblyStateCreateInfo;
+	this->stateInputAssembly.pNext = nullptr;
+	this->stateInputAssembly.flags = {};
+	this->stateInputAssembly.topology				= vk::PrimitiveTopology::ePointList;
+	this->stateInputAssembly.primitiveRestartEnable	= VK_FALSE;
 
+	this->createInfo.pInputAssemblyState = &this->stateInputAssembly;
 }
 
 void PipelineGraphic::createInfoStateTessellation(){
-
+	this->createInfo.pTessellationState = nullptr;
 }
 
 void PipelineGraphic::createInfoStateViewport(){
+	this->dummyViewport.x		= 0.0f;
+	this->dummyViewport.y		= 0.0f;
+	this->dummyViewport.width		= 1.0f;
+	this->dummyViewport.height	= 1.0f;
+	this->dummyViewport.minDepth	= 0.1f;
+	this->dummyViewport.maxDepth	= 1000.0f;
 
+	this->dummyScissor.offset.x	= 0;
+	this->dummyScissor.offset.y	= 0;
+	this->dummyScissor.extent.width	= 1;
+	this->dummyScissor.extent.height	= 1;
+
+	this->stateViewport.sType = vk::StructureType::ePipelineViewportStateCreateInfo;
+	this->stateViewport.pNext = nullptr;
+	this->stateViewport.flags = {};
+	this->stateViewport.viewportCount	= 1;
+	this->stateViewport.pViewports		= &this->dummyViewport;
+	this->stateViewport.scissorCount	= 1;
+	this->stateViewport.pScissors		= &this->dummyScissor;
+
+	this->createInfo.pViewportState = &this->stateViewport;
 }
 
 void PipelineGraphic::createInfoStateRasterization(){
+	this->stateRasterization.sType = vk::StructureType::ePipelineRasterizationStateCreateInfo;
+	this->stateRasterization.pNext = nullptr;
+	this->stateRasterization.flags = {};
+	this->stateRasterization.depthClampEnable			= VK_FALSE;
+	this->stateRasterization.rasterizerDiscardEnable	= VK_TRUE;
+	this->stateRasterization.polygonMode	= vk::PolygonMode::eFill;
+	this->stateRasterization.cullMode		= vk::CullModeFlagBits::eNone;
+	this->stateRasterization.frontFace		= vk::FrontFace::eCounterClockwise;
+	this->stateRasterization.depthBiasEnable			= VK_FALSE;
+	this->stateRasterization.depthBiasConstantFactor	= 0.0f;
+	this->stateRasterization.depthBiasClamp				= 0.0f;
+	this->stateRasterization.depthBiasSlopeFactor		= 0.0f;
+	this->stateRasterization.lineWidth					= 0.0f;
 
+	this->createInfo.pRasterizationState = &this->stateRasterization;
 }
 
 void PipelineGraphic::createInfoStateMultisample(){
-
+	this->createInfo.pMultisampleState = nullptr;
 }
 
 void PipelineGraphic::createInfoStateDepthStencil(){
-
+	this->createInfo.pDepthStencilState = nullptr;
 }
 
 void PipelineGraphic::createInfoStateColorBlend(){
-
+	this->createInfo.pColorBlendState = nullptr;
 }
 
 void PipelineGraphic::createInfoStateDynamic(){
-
+	this->createInfo.pDynamicState = nullptr;
 }
 
 
--- src/system/subsystem/graphics/vulkan/wrap/pipeline/pipelinegraphic.h
+++ src/system/subsystem/graphics/vulkan/wrap/pipeline/pipelinegraphic.h
@@ -22,6 +22,20 @@ namespace graphics{
 
 				std::weak_ptr<vk::Device> pwLogicDevice;
 
+				vk::PipelineShaderStageCreateInfo			stageShaderInfo{};
+				vk::PipelineVertexInputStateCreateInfo		stateVertexInput{};
+				vk::PipelineInputAssemblyStateCreateInfo	stateInputAssembly{};
+				vk::PipelineTessellationStateCreateInfo		stateTessellation{};
+				vk::PipelineViewportStateCreateInfo			stateViewport{};
+				vk::PipelineRasterizationStateCreateInfo	stateRasterization{};
+				vk::PipelineMultisampleStateCreateInfo		stateMultisample{};
+				vk::PipelineDepthStencilStateCreateInfo		stateDepthStencil{};
+				vk::PipelineColorBlendStateCreateInfo		stateColorBlend{};
+				vk::PipelineDynamicStateCreateInfo			stateDynamic{};
+
+				vk::Viewport	dummyViewport;
+				vk::Rect2D		dummyScissor;
+
 				void createInfoInit();
 				void createInfoStageShader();
 				void createInfoStateVertexInput();
