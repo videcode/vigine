d2e29f9841a88dc44f03f53c5f47a3fe72e64dba
videcode <videcode.link@gmail.com>
2021-08-15 19:01:27 +0300

    iObject add method

--- src/system/interface/iobject.h
+++ src/system/interface/iobject.h
@@ -2,6 +2,7 @@
 
 #include "api.h"
 #include API_INTERFACE_IBASE
+#include API_INTERFACE_IFIGURE
 
 
 namespace api {
@@ -10,7 +11,11 @@ namespace api {
 			iObject();
 			virtual ~iObject(){std::cout << "iObject destructor" << std::endl;}
 
+			virtual void						Figure(std::shared_ptr<iFigure> pFigureIn){this->pFigure = pFigureIn;}
+			virtual std::shared_ptr<iFigure>	Figure(){return this->pFigure;}
 		protected:
-
+			std::shared_ptr<iFigure>	pFigure{nullptr};
+			glm::vec3					position;
+			glm::mat4					matrix;
 	};
 }
